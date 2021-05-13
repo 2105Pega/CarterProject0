d30152f4afe68e87f3bdacd4d0bfd6ea669590ba
Kyle Carter <kmaxc3211@gmail.com>
2021-05-13 17:59:59 -0400

    Added JUnit testing and more menu features

--- src/main/java/com/revature/driver/Driver.java
+++ src/main/java/com/revature/driver/Driver.java
@@ -17,29 +17,54 @@ public class Driver {
 		// Hash Map of accounts (Primary key is accountNumber) with rel info
 		HashMap<Integer, Account> accounts = new HashMap<Integer, Account>();
 		
+		// Log in or sign up on the app
 		Scanner scan = new Scanner(System.in);
-	    System.out.println("Enter username:");
+		System.out.println("Welcome!\nWould you like to 'Log In' or 'Sign Up'?");
+		String response = scan.nextLine();
+		if (response.toLowerCase().startsWith("l")) {
+    		System.out.println("Enter username: ");
+    		String username = scan.nextLine();
+    		// If the username is associated with a user:
+    	    if (users.containsKey(username)) {
+    	    	// compare password associated with username
+    	    	System.out.println("Enter password:");
+    	    	String password = scan.nextLine();
+    	    	String expectedPassword = users.get(username).getPassword();
+    	    	if (password.equals(expectedPassword)) {
+    	    		System.out.println("Logging in...");
+    	    		browse(users.get(username));
+    	    	} else {
+    	    		System.out.println("Incorrect password :(");
+    	    		// go back and try again 
+    	    	}
+    	    	
+    	    } else {
+    	    	// no user with that username detected
+    	    	System.out.println("No account associated with that username.");
+    	    	// go back and try again
+    	    }
+    	} else if (response.toLowerCase().startsWith("s")) {
+    		// create new username
+    		System.out.println("Enter new username: ");
+    		String username = scan.nextLine();
+    		System.out.println("Enter new Password: ");
+    		String password = scan.nextLine();
+    		User temp = new User(username, password);
+    		users.put(username, temp);
+    		System.out.println("New User Created!");
+    		// auto-sign in and browse
+    		browse(temp);
+    	}
 	    
-	    String username = scan.nextLine();
-	    if(users.containsKey(username)) {
-	    	
-	    	System.out.println("Enter password:");
-	    	String password = scan.nextLine();
-	    } else {
-	    	System.out.println("No account associated with that username.");
-	    	System.out.println("Would you like to make and account? Y or N: ");
-	    	String response = scan.next();
-	    	if(response.toLowerCase().startsWith("y")) {
-	    		System.out.println("Gimme a minute...");
-	    	} 
-	    	else if (response.toLowerCase().startsWith("n")) {
-	    		System.out.println("Fine, be like that..");
-	    	}
-	    }
+	    
 	    
 	    System.out.println("Closing...");
 	    scan.close();
 	    System.out.println("Closed");
+	}
+	
+	public static void browse(User user) {
+		
 	}
 
 }
